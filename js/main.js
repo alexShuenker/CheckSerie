4e15dfcfc117e0cfa14ce70b50a6546f9ef7eca4
Alexsander Schuenker da Silva <alexsander.silva@outlook.com>
2023-10-26 13:54:42 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infitite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeitgh: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+});
